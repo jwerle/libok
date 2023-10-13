8a0fa67c5ee2b768055c7a2e4c563480c87ac132
Joseph Werle <joseph.werle@gmail.com>
2023-10-13 09:17:11 -0400

    refactor(ok.h): allow newlines to be optional in printf/fprintf

--- ok.h
+++ ok.h
@@ -18,9 +18,8 @@ extern "C" {
 /**
  * libok version
  */
-
 #ifndef OK_VERSION
-#define OK_VERSION "0.4.0"
+#define OK_VERSION "0.5.0"
 #endif
 
 /**
@@ -30,12 +29,32 @@ extern "C" {
 #define okx(...) (void) (0);
 #endif
 
+/**
+ * Allow for custom `printf` implementation.
+ */
 #if !defined(LIBOK_PRINTF)
 #define LIBOK_PRINTF(...) (printf(__VA_ARGS__))
 #endif
 
+/**
+ * Allow for custom `fprintf` implementation.
+ */
 #if !defined(LIBOK_FPRINTF)
 #define LIBOK_FPRINTF(...) (fprintf(__VA_ARGS__))
+#endif
+
+/**
+ * Configure the need for a newline ('\n') in a `printf` call.
+ */
+#if !defined(LIBOK_PRINTF_NEEDS_NEWLINE)
+#define LIBOK_PRINTF_NEEDS_NEWLINE 1
+#endif
+
+/**
+ * Configure the need for a newline ('\n') in a `fprintf` call.
+ */
+#if !defined(LIBOK_FPRINTF_NEEDS_NEWLINE)
+#define LIBOK_FPRINTF_NEEDS_NEWLINE LIBOK_PRINTF_NEEDS_NEWLINE
 #endif
 
 /**
@@ -45,7 +64,9 @@ extern "C" {
 #define ok(format, ...) ({                                                     \
   LIBOK_PRINTF("ok %d ", ok_count_inc());                                      \
   LIBOK_PRINTF(format, ##__VA_ARGS__);                                         \
-  LIBOK_PRINTF("\n");                                                          \
+  if (LIBOK_PRINTF_NEEDS_NEWLINE) {                                            \
+    LIBOK_PRINTF("\n");                                                        \
+  }                                                                            \
 })
 
 /**
@@ -54,65 +75,59 @@ extern "C" {
  * actual test count if the expected
  * count is greater than 0
  */
-
 void ok_done (void);
 
 /**
  * Sets the expectation count
  */
-
 void ok_expect (int);
 
 /**
  * Returns the expected count
  */
-
 int ok_expected (void);
 
 /**
  * Returns the ok count
  */
-
 int ok_count (void);
 int ok_count_inc (void);
 
 /**
  * Resets count and expected counters
  */
-
 void ok_reset (void);
 
 #if defined(LIBOK_INCLUDE_IMPLEMENTATION)
 
-/**
- * Represents the ok count
- */
-
 static int ok_count_;
-
-/**
- * Represents an optional
- * expected test count
- */
-
 static int ok_expected_;
 
 void ok_done (void) {
   if (0 != ok_expected() && ok_count() != ok_expected()) {
     if (ok_expected() > ok_count()) {
-      LIBOK_FPRINTF(stderr, "expected number of success conditions not met.\n");
+      LIBOK_FPRINTF(stderr, "expected number of success conditions not met.");
+      if (LIBOK_FPRINTF_NEEDS_NEWLINE) {
+        LIBOK_FPRINTF(stderr, "\n");
+      }
     } else {
       LIBOK_FPRINTF(stderr,
         "expected number of success conditions is less than the "
-        "number of given success conditions.\n");
+        "number of given success conditions.");
+      if (LIBOK_FPRINTF_NEEDS_NEWLINE) {
+        LIBOK_FPRINTF(stderr, "\n");
+      }
     }
     exit(1);
   }
 
-  LIBOK_PRINTF("1..%d\n", ok_count());
+  LIBOK_PRINTF("1..%d", ok_count());
+  if (LIBOK_PRINTF_NEEDS_NEWLINE) {
+    LIBOK_PRINTF("\n");
+  }
 }
 
-void ok_expect(int expected) {
+void ok_expect (int expected) {
   ok_expected_ = expected;
 }
 
